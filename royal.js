9a36d14c670604d557e41e5f31f198105a43e763
Peter5515 <142492452+Peter5515@users.noreply.github.com>
2023-09-04 23:26:06 +0530

    Add files via upload

---
+++ royal.js
@@ -0,0 +1,44 @@
+var motor=document.getElementById("m2")
+var a=()=>{
+    motor.src="./images/m6.avif"
+}
+var b=()=>{
+    motor.src="./images/motor4.avif"
+}
+var c=()=>{
+    motor.src="./images/motor5.avif"
+}
+var d=()=>{
+    motor.src="./images/super-meteor3.avif"
+}
+var e=()=>{
+    motor.src="./images/banner.avif"
+}
+var f=()=>{
+    motor.src="./images/royal-enfield-meteor-350.avif"
+}
+var g=()=>{
+    motor.src="./images/super-meteor3.avif"
+}
+// ************************************************************************
+
+var motor2=document.getElementById("m3")
+var a1=()=>{
+    motor2.src="./images/app2.avif"
+}
+var a2=()=>{
+    motor2.src="./images/app3.avif"
+}
+var a3=()=>{
+    motor2.src="./images/app4.avif"
+}
+var a4=()=>{
+    motor2.src="./images/app5.avif"
+}
+var a5=()=>{
+    motor2.src="./images/app6.avif"
+}
+var a6=()=>{
+    motor2.src="./images/image2.avif"
+}
+
